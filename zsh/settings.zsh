78b95b65ca4cbb856503c86ae471c2cde98b72ac
Wu Erqi <rafe@roborock.com>
2021-07-07 11:29:04 +0800

    A setting file for z shell

---
+++ zsh/settings.zsh
@@ -0,0 +1,21 @@
+# Initialize completion
+autoload -Uz compinit && compinit -i
+zstyle ':completion:*' menu select=4
+zmodload zsh/complist
+
+# Nicer history
+HISTSIZE=1048576
+HISTFILE="$HOME/.zsh_history"
+SAVEHIST=$HISTSIZE
+setopt appendhistory
+setopt incappendhistory
+setopt extendedhistory
+
+# Time to wait for additional characters in a sequence
+KEYTIMEOUT=1  # corresponds to 10ms
+
+
+# Editor choose
+export EDITOR=vim
+
+
